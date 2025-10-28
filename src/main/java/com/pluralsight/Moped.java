fd80bb354ede91c2cc2fa1c44a239543eeb34280
arsenii <arseniy.kunilovskiy@gmail.com>
2025-10-28 09:50:45 -0400

    Constructors are added

--- src/main/java/com/pluralsight/Moped.java
+++ src/main/java/com/pluralsight/Moped.java
@@ -3,6 +3,10 @@ package com.pluralsight;
 public class Moped extends Vehicle{
     private int maxWeight;
 
+    public Moped(String model, String color, int topSpeed, int fuelCapacity, int numberOfPassengers, int cargoCapacity, int maxWeight){
+        super(model, color,topSpeed,fuelCapacity,numberOfPassengers,cargoCapacity);
+        this.maxWeight = maxWeight;
+    }
     public void ride(){
         System.out.println("ride");
     }
